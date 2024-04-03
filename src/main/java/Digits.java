9e1ebfec3e72fb2dc1f0efe121dc6e6e96f4ed98
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-04-03 11:57:15 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -15,9 +15,13 @@ public class Digits
 
 	public boolean isStrictlyIncreasing()
 	{ 
-		//wip
+		for(int i = 0; i < digitList.size() - 1; i++){
+			if(digitList.get(i) > digitList.get(i + 1)){
+				return false;
+			}
+		}
 		
-
+		return true;
 	}
 	
 	public String toString()
